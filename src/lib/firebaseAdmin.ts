b87d0dc6bc9fdf90f2384b4dc07bf993be0a7203
Alex <korte@clemson.edu>
2024-06-14 23:28:09 -0700

    Ok maybe I did still need that

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -3,11 +3,15 @@ import { env } from "$env/dynamic/private";
 
 console.log("env.FIREBASE_ADMIN_KEY", env.FIREBASE_ADMIN_KEY);
 
-if (!admin.apps.length) {
-	const serviceAccount = JSON.parse(env.FIREBASE_ADMIN_KEY);
-	admin.initializeApp({
-		credential: admin.credential.cert(serviceAccount),
-	});
+try {
+	if (!admin.apps.length) {
+		const serviceAccount = JSON.parse(env.FIREBASE_ADMIN_KEY);
+		admin.initializeApp({
+			credential: admin.credential.cert(serviceAccount),
+		});
+	}
+} catch (error) {
+	console.log("Firebase admin initialization error", error.stack);
 }
 
 export default admin;
