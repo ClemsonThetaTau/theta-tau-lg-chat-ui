20ed27124da3e07d9a187ba3e664a1db2f16ba7f
Alex <korte@clemson.edu>
2024-06-14 23:18:11 -0700

    I think I can remove this

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -3,15 +3,11 @@ import { env } from "$env/dynamic/private";
 
 console.log("env.FIREBASE_ADMIN_KEY", env.FIREBASE_ADMIN_KEY);
 
-try {
-	if (!admin.apps.length) {
-		const serviceAccount = JSON.parse(env.FIREBASE_ADMIN_KEY);
-		admin.initializeApp({
-			credential: admin.credential.cert(serviceAccount),
-		});
-	}
-} catch (error) {
-	console.log("Firebase admin initialization error", error.stack);
+if (!admin.apps.length) {
+	const serviceAccount = JSON.parse(env.FIREBASE_ADMIN_KEY);
+	admin.initializeApp({
+		credential: admin.credential.cert(serviceAccount),
+	});
 }
 
 export default admin;
